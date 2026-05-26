994e401f4309b44649ef9cb75f59d83cae14c602
BigGubson <meekpungie@hotmail.com>
2026-05-26 15:36:50 -0700

    taking a break for the day

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+from player import Player
 from logger import log_state
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 VERSION = pygame.version.ver
@@ -9,12 +10,19 @@ def main():
 	print(f"Screen height: {SCREEN_HEIGHT}")
 	pygame.init()
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+	clock = pygame.time.Clock()
+	dt = 0.0
+	player = Player(SCREEN_WIDTH /2, SCREEN_HEIGHT / 2)
 	while True:
+		log_state()
+		dt = (clock.tick(60)/1000)
+		screen.fill("black")
+		player.update(dt)
+		player.draw(screen)
+		pygame.display.flip()
 		for event in pygame.event.get():
     			if event.type == pygame.QUIT:
         			return
-	screen.fill(black)
-	pygame.display.flip()
 
 if __name__ == "__main__":
     main()
